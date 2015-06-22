ff0fd3c787da3971a10db3bacd065b474339a767
Pranavan <234rana@gmail.com>
2015-06-22 23:08:17 +0530

    IO Layer skeleton added

---
+++ src/com/morph/io/IOLayer.java
@@ -0,0 +1,28 @@
+package com.morph.io;
+
+import com.morph.tamil.TamilFontEntity;
+
+import java.util.List;
+
+/**
+ * Created by pranavan on 22/06/15.
+ */
+public class IOLayer {
+    /**
+     * need to implement
+     * @param text
+     * @return
+     */
+    public static List<TamilFontEntity> getTamil(String text){
+        return null;
+    }
+
+    /**
+     * need to implement
+     * @param list
+     * @return
+     */
+    public static String getText(List<TamilFontEntity> list){
+        return null;
+    }
+}
