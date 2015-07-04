2001a89004e537b82773be9399ce0a0d5af113fd
megala21 <ktumegala@yahoo.com>
2015-07-04 23:02:03 +0530

    Refactoring of the some very known pattern matching rules

---
+++ src/main/java/com/org/morph/noun/NounRule.java
@@ -0,0 +1,111 @@
+package com.org.morph.noun;
+
+import com.org.morph.io.IOLayer;
+import com.org.morph.tamil.TamilFontEntity;
+
+import java.util.List;
+
+/**
+ * Created by megala on 7/3/15.
+ * This class keeps the details of a single noun rule which is used find out the nouns based on its endings
+ */
+public class NounRule {
+    // Minimum length of the word required
+    private int minimumLength;
+    // Ending pattern to be checked
+    private String pattern;
+    // The x coordinate of the letter before the pattern
+    private int xCoordinateBefore;
+    // The y coordinate of the letter before the pattern
+    private int yCoordinateBefore;
+    // The number of tamil characters in the pattern
+    private int numberOfLastCharactersToBeChecked;
+
+    /**
+     * This constructs a noun rule
+     * @param minimumLength Minimum length of the word
+     * @param pattern Pattern to be checked at the end of the word
+     * @param x X coordinate of the letter before the pattern
+     * @param y Y coordinate of the letter before the pattern
+     * @param numberOfLastCharactersToBeChecked Number of characters in the pattern
+     */
+    public NounRule(int minimumLength, String pattern, int x, int y, int numberOfLastCharactersToBeChecked){
+        this.minimumLength = minimumLength;
+        this.pattern = pattern;
+        xCoordinateBefore = x;
+        yCoordinateBefore = y;
+        this.numberOfLastCharactersToBeChecked = numberOfLastCharactersToBeChecked;
+
+    }
+
+    /**
+     * To get the minimum length of the word required to check this pattern
+     * @return the minimum length of the word
+     */
+    public int getMinimumLength() {
+        return minimumLength;
+    }
+
+    /**
+     * Number of tamil characters in the last pattern
+     * @return the number of tamil characters in the pattern to be checked
+     */
+    public int getNumberOfLastCharactersToBeChecked() {
+        return numberOfLastCharactersToBeChecked;
+    }
+
+    /**
+     * Checks whether the given word satisfies the specific pattern
+     * @param tamilWord Tamil word to be checked
+     * @return true if the word satisfies the given pattern, otherwise false
+     */
+    public boolean checkPattern(List<TamilFontEntity> tamilWord) {
+        if(IOLayer.getTamil(pattern).equals(tamilWord))
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Checks whether the x coordinate of the letter before the pattern matches with the the given character`s x coordinate
+     * @param tamilCharacter Tamil character to be checked
+     * @return true if the x coordinate matches, false otherwise
+     */
+    public boolean isXCoordinateMatches(TamilFontEntity tamilCharacter){
+        if(xCoordinateBefore == -1)
+            return true;
+        else{
+            if(xCoordinateBefore == tamilCharacter.getxLocation()){
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+    /**
+    * Checks whether the y coordinate of the letter before the pattern matches with the the given character`s y coordinate
+    * @param tamilCharacter Tamil character to be checked
+    * @return true if the y coordinate matches, false otherwise
+    */
+    public boolean isYCoordinateMatches(TamilFontEntity tamilCharacter){
+        if(yCoordinateBefore == -1)
+            return true;
+        else{
+            if (yCoordinateBefore == tamilCharacter.getyLocation()){
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+    /**
+     * This method checks whether the this rule requires to select the before character of the pattern
+     * @return true if it need to be checked otherwise false
+     */
+    public boolean isbeforePositionToBeChecked(){
+        return !(xCoordinateBefore == -1 && yCoordinateBefore == -1);
+    }
+
+}
